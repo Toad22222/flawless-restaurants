ab6c04a33e31d4807fdb433d67962151d4a7387f
Toad22222 <sychen23@yahoo.com>
2017-04-22 18:27:03 -0400

    prompting preferences and displaying matches

--- restaurants.py
+++ restaurants.py
@@ -1,81 +1,125 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 22 15:37:58 2017
-
-@author: rebeccabrunsberg
-"""
 
 from collections import defaultdict
 import csv
 
+attributes = ['name', 'loc', 'time', 'price', 'dist', 'cuisine']
+pref_dict = {'name': [], 'loc': [], 'time': [], 'price': [],
+            'dist': [], 'cuisine': []}
+
+
 class Restaurant(object):
+   def __init__(self, name, loc, time, price, dist, cuisine):
+       self.name = str(name)
+       self.loc = str(loc)
+       self.time = str(time)
+       self.price = str(price)
+       self.dist = str(dist)
+       self.cuisine = str(cuisine)
 
+   def __str__(self):
+       return self.name
 
-    def __init__(self, name, loc, time, price, dist, cuisine):
-        self.name = name
-        self.loc=loc
-        self.time=time
-        self.price=price
-        self.dist=dist
-        self.cuisine=cuisine
-        self.fieldnames = ['name', 'loc', 'time', \
-        'price', 'dist','cuisine']
-
-    def __str__(self):
-        return self.name
 
 def readFile(filename, rlist):
-    restaurants= open(filename, 'r')
-    for line in restaurants:
-        data=line.split(',')
-        this_rest = Restaurant(data[0], data[1], data[2], data[3], \
-                               data[4], data[5])
-        rlist.append(this_rest)
+   with open('rest.csv') as csvfile:
+       reader = csv.DictReader(csvfile)
+       for row in reader:
+           this_rest = Restaurant(
+               row['name'], row['loc'], row['time'],
+               row['price'], row['dist'], row['cuisine'])
+           rlist.append(this_rest)
 
-def enterNew(filename, rlist):
-    response='y'
-    r =Restaurant(1,1,1,1,1,1)
-    fields=r.fieldnames
-    with open(filename, 'a') as rest:
 
-        while (response=='y'):
-            name=input("Please enter the name: ")
-            loc=input("location? ")
-            time=input("average time to eat there? (in minutes) ")
-            price=input("average price for 1: ")
-            dist=input("distance from your home (in miles) ")
-            cuisine=input("cuisine? separate by spaces ")
-            rlist.append(Restaurant(name, loc, time, price, dist, cuisine))
+def enterNew(rlist):
+   name = input("Please enter the name: ")
+   loc = input("location? ")
+   time = input("average time to eat there? (in minutes) ")
+   price = input("average price for 1: ")
+   dist = input("distance from your home (in miles) ")
+   cuisine = input("cuisine? separate by spaces ")
+   rlist.append(Restaurant(name, loc, time, price, dist, cuisine))
 
-            writer = csv.DictWriter(rest,fieldnames=fields)
-            writer.writerow({fields[0]:name, fields[1]:loc, fields[2]:time, \
-                            fields[3]:price, fields[4]:dist, fields[5]:cuisine})
-            response=input("Would you like to add more restaurants y/n ")
 
 def main():
-    use = input("What would you like to do?)
-    rlist = []
-    #db="rest.csv"
-    with open('rest.csv', 'r') as db:
-        reader=csv.DictReader(db)
-        for row in reader:
-            rlist.append(Restaurant(row['name'], row['loc'], \
-                                    row['time'], row['price'], \
-                                    row['dist'], row['cuisine']))
-    #readFile(db, rlist)
-    print("Here is a list of your current restaurants")
-    for restaurant in rlist:
-        print(str(restaurant))
-    response = input("Would you like to add more restaurants y/n ")
-    if (response == 'y'):
-        enterNew('rest.csv', rlist)
-        response=input("Would you like to add more restaurants y/n ")
-    print("Here is a list of your current restaurants")
-    for restaurant in rlist:
-        print(str(restaurant))
+   use =input(“What would you like to do?\n a) get suggestions \
+, b)
+   menu_text = '''What do you have a preference for?
+   1) name
+   2) location
+   3) time
+   4) price
+   5) distance
+   6) cuisine
+   Enter a number: '''
+   add_pref = True
 
+   while add_pref:
+       pref_num = input(menu_text)
+       print("")
+       print("preferences: " + str(pref_dict))
+       print("")
+       try:
+           pref_int = int(pref_num) - 1
+           ask_pref(pref_int)
+       except ValueError:
+           print('Sorry, please enter that again.')
+       except IndexError:
+           print('Sorry, please enter that again.')
+       done_add_pref = input('Done adding preferences? (Y/N): ' )
+       if done_add_pref.upper() == 'Y':
+           add_pref = False
+
+   print("")
+   print("preferences: " + str(pref_dict))
+   print("")
+
+   get_suggestions()
+   print('done with main :D')
+
+
+def ask_pref(pref_int):
+   pref_text = input("Enter your preference for " + attributes[pref_int] + ": ")
+   attribute = attributes[pref_int]
+   pref_dict[attribute].append(pref_text)
+
+
+def find_matches(rlist, threshold):
+   count = 0
+   for restaurant in rlist:
+       match = 0
+       if restaurant.name in pref_dict['name']:
+           match += 1
+       if restaurant.loc in pref_dict['loc']:
+           match += 1
+       if restaurant.time in pref_dict['time']:
+           match += 1
+       if restaurant.dist in pref_dict['dist']:
+           match += 1
+       if restaurant.cuisine in pref_dict['cuisine']:
+           match += 1
+       if match >= threshold:
+           print(str(restaurant))
+           count += 1
+   if count == 0:
+       print("No matches found")
+
+
+def get_suggestions():
+   threshold = len(pref_dict)
+   rlist = []
+   readFile("rest.csv", rlist)
+   print("Here are your restaurant recommendations")
+   find_matches(rlist, threshold)
+   response = input("Would you like more restaurant recommendations? y/n ")
+   while response == 'y':
+       threshold -= 1
+       find_matches(rlist, threshold)
+       response = input("Would you like more restaurant recommendations? y/n ")
 
 
 if __name__ == "__main__":
-    main()
+   main()
+
+
